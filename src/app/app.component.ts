052483f5074c687b008f1acd40692d40fb68a841
Rafael Neves <rafaelneves652@gmail.com>
2021-04-26 19:55:22 -0300

    added gh pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
-import {OwlOptions} from 'ngx-owl-carousel-o'
+import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  constructor() { }
+export class AppComponent implements OnInit {
+  images: string[] = [
+    'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/c1k1N5rrA6NGhKUZ6hOYR0Am9Ss.jpg',
+    'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/w8Hi3GI4q1oR6EImrDWrAQFn8Ha.jpg',
+    'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/vGIIl89vglo66yUfbuTxzNAs4y5.jpg',
+    'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/Ar8jquseIADaFtMH6dQpskLwDYf.jpg',
+    'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/n8eF3o2ldX4ifR5m4G51cjClBP5.jpg',
+    'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ArWn6gCi61b3b3hclD2L0LOk66k.jpg'
+  ];
+
+  constructor() {}
 
   customOptions: OwlOptions = {
     loop: true,
@@ -20,18 +29,20 @@ export class AppComponent {
     navText: ['&#8249', '&#8250;'],
     responsive: {
       0: {
-        items: 1
+        items: 2,
       },
-      400: {
-        items: 2
-      },
-      760: {
-        items: 3
+      600: {
+        items: 3,
       },
       1000: {
-        items: 4
+        items: 5,
       }
     },
+  };
 
+  ngOnInit() {
+    for (let i = 0; i < this.images.length; i++) {
+      let image = this.images[i];
+    }
   }
 }
